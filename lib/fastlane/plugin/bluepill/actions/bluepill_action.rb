78490cebf50b8d45d9c640fad8ce2d0c1849fc9f
tbrand <taichir0709@gmail.com>
2017-10-02 23:38:03 +0900

    fix option

--- lib/fastlane/plugin/bluepill/actions/bluepill_action.rb
+++ lib/fastlane/plugin/bluepill/actions/bluepill_action.rb
@@ -4,9 +4,9 @@ module Fastlane
       def self.run(params)
         UI.message("Start test by using Bluepill: https://github.com/linkedin/bluepill")
         cmd =  bin_bluepill.to_s
+        cmd << " #{params[:xctestrun]}"
         cmd << " -a #{params[:app]}"
         cmd << " -o #{params[:output_dir]}"
-        cmd << " -s #{params[:scheme]}"
         cmd << " -d \"#{params[:device]}\""
         cmd << " -n #{params[:number_of_simulators]}"
         cmd << " -H" if params[:headless]
@@ -35,9 +35,9 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :scheme,
-                                       env_name: "BLUEPILL_SCHEME_PATH",
-                                       description: "scheme path (.xcscheme)",
+          FastlaneCore::ConfigItem.new(key: :xctestrun,
+                                       env_name: "BLUEPILL_XCTESTRUN",
+                                       description: "The path to the .xctestrun file that xcode leaves",
                                        optional: false,
                                        is_string: true),
           FastlaneCore::ConfigItem.new(key: :output_dir,
--- spec/bluepill_action_spec.rb
+++ spec/bluepill_action_spec.rb
@@ -3,19 +3,19 @@ describe Fastlane::Actions::BluepillAction do
     it 'Execute bluepill' do
       allow(Fastlane::UI).to receive(:message)
 
+      option_t = "dummy.xctestrun"
       option_a = "dummy.app"
-      option_s = "dummy.xcscheme"
       option_o = "dummy_dir"
       option_d = "iPhone 6"
       option_n = 4
       option_h = true
       option_r = true
 
-      expect(Fastlane::Actions::BluepillAction).to receive(:sh).with("#{Fastlane::Actions::BluepillAction.bin_bluepill} -a #{option_a} -o #{option_o} -s #{option_s} -d \"#{option_d}\" -n #{option_n} -H --reuse-simulator")
+      expect(Fastlane::Actions::BluepillAction).to receive(:sh).with("#{Fastlane::Actions::BluepillAction.bin_bluepill} #{option_t} -a #{option_a} -o #{option_o} -d \"#{option_d}\" -n #{option_n} -H --reuse-simulator")
 
       Fastlane::Actions::BluepillAction.run({
+                                              xctestrun: option_t,
                                               app: option_a,
-                                              scheme: option_s,
                                               output_dir: option_o,
                                               device: option_d,
                                               number_of_simulators: option_n,
